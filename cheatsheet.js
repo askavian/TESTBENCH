06b26c25fab88e386db2d978cb5e90a5500332e2
askavian <malte.boettcher@live.com>
2025-01-04 01:52:46 +0100

    feat:

--- cheatsheet.js
+++ cheatsheet.js
@@ -50,8 +50,118 @@ if (money && time) {
 }
 
 
-// BASIC LOOP 
+//FOR LOOP 
 
 for (i = 0; i <= 10; i++) {
     console.log(i);
   }
+
+
+//ARRAY LIST
+
+let fruits = ['apple', 'orange', 'banana', 'pear', 'plum'];
+
+// Print all fruits
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+    
+// Reverse the list
+fruits.reverse();
+console.log(fruits);
+
+// Sort the list alphabetically:
+fruits.sort();
+console.log(fruits);
+
+// Access ONE Item (Index)
+let flowers = ["daisy", "bluebell", "buttercup", "rose", "tulip", "daffodil", "orchid", "geranium", "heather", "hydrangea"];
+let flower = flowers[5];
+console.log(flowers);
+console.log(flower);
+
+
+//OBJECTS DICTIONARIES
+
+// Create the object
+let person = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 32,
+  };
+  
+  // Print its properties using dot notation
+  console.log(person.firstName);
+  console.log(person.lastName);
+  console.log(person.age);
+  
+  // Or using square bracket notation
+  console.log(person['firstName']);
+  console.log(person['lastName']);
+  console.log(person['age']);
+
+
+//SETS
+
+  // Create a set
+let directions = new Set(['north', 'south', 'east', 'west']);
+
+// Display the set
+console.log(directions);
+
+// Add an item to the set:
+directions.add('northwest');
+
+// Display it again
+console.log(directions);
+
+
+//Nested & Multi-dimensional Data Structures
+
+// Create an array of contact objects
+let contacts = [
+    {
+      name: 'Joe',
+      phoneNumber: '123456789'
+    },
+    {
+      name: 'Mary',
+      phoneNumber: '456789123'
+    },
+    {
+      name: 'Dad',
+      phoneNumber: '789132456'
+    },
+  ];
+  
+  // Print each contact using its array index
+  console.log(contacts[0]);
+  console.log(contacts[1]);
+  console.log(contacts[2]);
+
+
+//Navigation & Iteration of Data Structures
+
+// Create an array of arrays
+let controls = [
+    ['up', 'down', 'left', 'right'],
+    ['a', 'b', 'select', 'start']
+  ];
+  
+  // Log the first sub-array
+  console.log(controls[0]);  
+  
+  // Log the second sub-array
+  console.log(controls[1]);
+  
+  // In the first sub-array (index 0) each item is a direction
+  console.log(controls[0][0]);  // logs 'up'
+  console.log(controls[0][1]);  // logs 'down'
+  console.log(controls[0][2]);  // logs 'left'
+  console.log(controls[0][3]);  // logs 'right'
+  
+  // In the second sub-array (index 1) each item is a button
+  console.log(controls[1][0]);  // logs 'a'
+  console.log(controls[1][1]);  // logs 'b'
+  console.log(controls[1][2]);  // logs 'select'
+  console.log(controls[1][3]);  // logs 'start'
--- cheatsheet.py
+++ cheatsheet.py
@@ -53,7 +53,7 @@ else:
   print('Good Luck Brother!')
 
 
-  #TEST LOOP
+#FOR LOOP
 
 my_string = "iterate over this"
 
@@ -62,3 +62,113 @@ for letter in my_string:
 
 for i, letter in enumerate(my_string):
     print(i, letter)
+
+
+#ARRAY LIST
+
+fruits = ['apple', 'orange', 'banana', 'pear', 'plum']
+
+# Print all fruits
+for fruit in fruits:
+    print(fruit)
+
+print()
+
+# Get an item located in a list
+second_item = fruits[1]
+print(second_item)
+print()
+
+# Add an item to the list
+fruits.append('cherries')
+print(fruits)
+print()
+
+# Reverse the list
+fruits.reverse()
+print(fruits)
+
+# Sort the list alphabetically:
+fruits.sort()
+print(fruits)
+
+# Access ONE Liste Item
+flowers = ["daisy", "bluebell", "buttercup", "rose", "tulip", "daffodil", "orchid", "geranium", "heather", "hydrangea"]
+flower = flowers[9]
+print(flowers)
+print(flower)
+
+
+#OBJECT/DICTIONARIES
+
+# Create the dictionary
+my_dict = {
+  'some_number': 12,
+  'a_string': 'Hello world!',
+  'nested_list': [1, 2, 3],
+  'a_boolean': True,
+}
+
+# Print its contents one by one
+print(my_dict['some_number'])
+print(my_dict['a_string'])
+print(my_dict['nested_list'])
+print(my_dict['a_boolean'])
+
+
+#SETS
+
+# Create a set
+directions = set(['north', 'south', 'east', 'west'])
+
+# Print its members
+for direction in directions:
+    print(direction)
+
+# Add an item to the set:
+directions.add('northwest')
+
+print()
+# Print the members again
+# Notice the order cannot be relied upon!
+for direction in directions:
+    print(direction)
+
+
+#Nested & Multi-dimensional Data Structures
+
+# Create a list of foods by food group
+foods = {
+  'dairy': ['milk', 'cheese', 'yogurt'],
+  'grains': ['oats', 'cereal', 'pasta'],
+  'fats_and_sweets': ['chocolate', 'sugar', 'butter'],
+  'fruits': ['apples', 'oranges', 'bananas'],
+  'vegetables': ['broccoli', 'beans', 'peas']
+}
+
+# Print each group
+print(foods['dairy'])
+print(foods['grains'])
+print(foods['fats_and_sweets'])
+print(foods['fruits'])
+print(foods['vegetables'])
+
+
+#Navigation & Iteration of Data Structures
+
+# Create a dictionary of food groups
+foods = {
+  'dairy': ['milk', 'cheese', 'yogurt'],
+  'grains': ['oats', 'cereal', 'pasta'],
+  'fats_and_sweets': ['chocolate', 'sugar', 'butter'],
+  'fruits': ['apples', 'oranges', 'bananas'],
+  'vegetables': ['broccoli', 'beans', 'peas']
+}
+
+# Print the entire dairy group
+print(foods['dairy'])
+
+# Print specific foods from the dairy group
+print(foods['dairy'][0]) 
+print(foods['dairy'][1])
+print(foods['dairy'][2])
