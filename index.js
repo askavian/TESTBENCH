e47f1e1974e3785e603324d784029f1a789157a9
askavian <malte.boettcher@live.com>
2025-01-05 14:49:14 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-let result = true;
-let city = 'Dublin';
-let firstResponse;
-let secondResponse;
-
-if(result) {
-	firstResponse = 'result is true';
-}
-
-if(city) {
-	secondResponse = 'Thank you for choosing a city';
-} else {
-	secondResponse = 'You need to fill in the name of a City';
-}
-
-console.log(firstResponse);
-console.log(secondResponse);
+let variable = 42;
+console.log(variable);
