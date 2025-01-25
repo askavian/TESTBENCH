d170734b17b54b3876667804855d1ebcaee1c5c5
askavian <malte.boettcher@live.com>
2025-01-25 22:02:22 +0100

    feat:

---
+++ main.py
@@ -0,0 +1,15 @@
+age = 77 # If below 18 Script ENDS here
+if age <= 18:
+    raise SystemExit('You must be older than 18!') # raise SystemExit shuts down code when condition is met
+
+
+print("Hello world!") # SINGLE VALUE
+print("Please", "say", "hello", "back!") # MULTIPLE VALUES
+print("I am here to learn!") # SINGLE VALUE
+
+
+my_number = 5 # DECLARING VARIABLES
+print(my_number)
+
+
+
